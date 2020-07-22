8ad956bc8150d743edf7e85b65d686b4a4c69488
Tarik Koric <tarik0219@gmail.com>
2020-07-21 20:42:36 -0500

    added legend and made into folder

--- test/scatter_plot.js
+++ test/scatter_plot.js
@@ -1,4 +1,7 @@
 // set the dimensions and margins of the graph
+
+
+
 var margin = {top: 10, right: 30, bottom: 100, left: 100},
     width = 1000 - margin.left - margin.right,
     height = 1000 - margin.top - margin.bottom;
@@ -12,9 +15,11 @@ var svg = d3.select("#my_dataviz")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-//Read the data
-d3.csv("cbb_2020.csv", function(data) {
 
+
+
+d3.csv("cbb_2020.csv", function(data) {
+  console.log(data)
   // Add X axis
   var x = d3.scaleLinear()
     .domain([0, 100])
@@ -36,6 +41,7 @@ d3.csv("cbb_2020.csv", function(data) {
       .append("div")
       .style("position", "absolute")
       .style("visibility", "hidden")
+      
 
 
   var color = d3.scaleOrdinal()
@@ -45,7 +51,7 @@ d3.csv("cbb_2020.csv", function(data) {
   // A function that change this tooltip when the user hover a point.
   // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
   var mouseover = function(d) {
-    tooltip.style("visibility", "visible")
+    tooltip.style("visibility", "visible").style("background-color", "gray")
 
     selected_specie = d.CONF
   
@@ -65,7 +71,7 @@ d3.csv("cbb_2020.csv", function(data) {
   var mousemove = function(d) {
     tooltip
     .html("Team: " + d.TEAM + "</br>" + "Conference: " + d.CONF + "</br>" + "Win % : " + Math.round((((+ d.W)/ (+ d.G)) * 10000))/100)
-    .style("top", (event.pageY-800)+"px").style("left",(event.pageX-800)+"px")
+    .style("top", (event.pageY + 100)+"px").style("left",(event.pageX + 100)+"px")
   }
 
   // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
@@ -100,7 +106,7 @@ d3.csv("cbb_2020.csv", function(data) {
     .attr("y", height + 50)
     .text("Win %");
 
-    svg.append("text")
+  svg.append("text")
     .attr("class", "y label")
     .attr("text-anchor", "end")
     .attr("y", -60)
@@ -109,6 +115,20 @@ d3.csv("cbb_2020.csv", function(data) {
     .attr("transform", "rotate(-90)")
     .text("Three Point FG %");
 
+
+    svg.append("circle").attr("cx",200).attr("cy",130).attr("r", 6).style("fill", "red")
+    svg.append("circle").attr("cx",200).attr("cy",150).attr("r", 6).style("fill", "blue")
+    svg.append("circle").attr("cx",200).attr("cy",170).attr("r", 6).style("fill", "green")
+    svg.append("circle").attr("cx",200).attr("cy",190).attr("r", 6).style("fill", "orange")
+    svg.append("circle").attr("cx",200).attr("cy",210).attr("r", 6).style("fill", "yellow")
+    svg.append("circle").attr("cx",200).attr("cy",230).attr("r", 6).style("fill", "black")
+    svg.append("text").attr("x", 220).attr("y", 130).text("Big Ten").style("font-size", "15px").attr("alignment-baseline","middle")
+    svg.append("text").attr("x", 220).attr("y", 150).text("ACC").style("font-size", "15px").attr("alignment-baseline","middle")
+    svg.append("text").attr("x", 220).attr("y", 170).text("Big East").style("font-size", "15px").attr("alignment-baseline","middle")
+    svg.append("text").attr("x", 220).attr("y", 190).text("Big 12").style("font-size", "15px").attr("alignment-baseline","middle")
+    svg.append("text").attr("x", 220).attr("y", 210).text("Pac 12").style("font-size", "15px").attr("alignment-baseline","middle")
+    svg.append("text").attr("x", 220).attr("y", 230).text("SEC").style("font-size", "15px").attr("alignment-baseline","middle")
+      
   
     
     
